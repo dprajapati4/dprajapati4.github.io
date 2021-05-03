638bd450daa41d124feaa96b4e4fc68840218bf3
Deep Prajapati <dprajap2@gmail.com>
2021-05-03 11:33:58 -0400

    Adds project descriptions and basic css

--- src/App.js
+++ src/App.js
@@ -1,52 +1,30 @@
 import React from 'react';
-import './App.css';
-import pdf from './data/resume.pdf';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
+import './App.css';
+import Home from './components/Home';
+import Navbar from "./components/Navbar"
+import AboutMe from "./components/AboutMe"
+import Footer from "./components/Footer"
+import Projects from './components/Projects';
+import Resume from "./components/Resume"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Deep Prajapati </h1>
-        <div className="about-me">
-          <h3> Hello!</h3>
-          <h4>Who I am</h4>
-          <p> I am a New York City based Software Engineer who is passionate about solving problems with simple solutions
-            </p>
-            <h4> Where I come from</h4>
-            <p> Curious about the biology and psychology of the human mind
-              I graduated with a BSc in Neuroscience.</p>
-              <p> As a research assistant, I worked with 3D printed bioceramic scaffolds that aimed to augement calvaria bone growth. </p>
-              <p>I analyzed psychometric scales and statistical data as a psychometrician to help diagnose psychological issues and learning disabilities. </p>
-              <p>I graduated from The Grace Hopper Program at Fullstack Academy and develop the skills I needed to learn quickly
-          and efficiently</p>
-        </div>
-        <span>Connect With Me</span>
-        <a
-          className="github-link"
-          href="https://www.github.com/dprajapati4"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          github
-        </a>
-        <a
-          className="linkedIn-link"
-          href="https://www.linkedin.com/in/deepprajapati/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          linkedIn
-        </a>
-        <a
-          className="link black-80 hover-dark-gray pb2 pr2 f6"
-          href={pdf}
-          download="Prajapati_Deep_Resume.pdf"
-        >
-          resume
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <Navbar/>
+    <Switch>
+      <Route exact path ="/" component={Home} />
+      <Route exact path ="/projects" component={Projects} />
+      <Route exact path ="/resume" component={Resume}/>
+    </Switch>
+    <Footer/>
+
+
+
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from "./Navbar"
+import AboutMe from "./AboutMe"
+import Footer from "./Footer"
+
+
+const Home = () => {
+  return(
+    <div className="Home">
+
+<AboutMe/>
+
+</div>
+  )
+}
+
+export default Home
