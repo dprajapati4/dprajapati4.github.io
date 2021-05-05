9619ac32b6be53fb6cc741346931e46b54ca1984
Deep Prajapati <dprajap2@gmail.com>
2021-05-05 18:49:25 -0400

    Updates projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,9 @@ const Projects = () => {
         A tribute to my love for Parks and Recreation, a character Gif generator. Choose between Leslie, Ron, Anne and other main characters and generate a one line from the show.
 
       </div>
+      <div className="Code-brew">
+        Recreating a cafe experience, Cafe Code Brew allows its users to select a cafe ambience, music, view real cafes near you and see ratings and address and order food. Use the onsite button to message your friends and invite them to your cafe experience. 
+      </div>
       <div className="Grace-Shopper">
         An fun e-commerce website that sells all inclusive tickets to intergalatic stars.
 
