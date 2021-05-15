6f4466fc02f0be03cad4eaea3ff003a028271cf1
Deep Prajapati <dprajap2@gmail.com>
2021-05-15 19:00:22 -0400

    adds gifs

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,28 +2,28 @@ const projectData = [
 {
     title:"1. Parks and Recreation Gif Generator",
     description:"A tribute to my love for Parks and Recreation, a character Gif generator. Choose between Leslie, Ron, Anne and other main characters and generate a gif from the show.",
-    url:"https://deeps-personal-website.s3.us-east-2.amazonaws.com/Parks+and+Rec+Gif.gif",
+    url:`${process.env.PUBLIC_URL}/assets/ParksandRec.gif`,
     alt:"parks and ref gif",
     technology:["React.js", "Node.js", "Html","CSS", "Giphy API"]
   },
  {
       title:"2. Cafe Code Brew",
       description:"Recreating a cafe experience, Cafe Code Brew allows its users to select a cafe ambience, music, view real cafes near you and order food. Use the onsite messaging to invite your friends to your cafe experience.",
-      url:"https://deeps-personal-website.s3.us-east-2.amazonaws.com/Code+Brew+Gif.gif",
+      url:`${process.env.PUBLIC_URL}/assets/CodeBrew.gif`,
       alt:"Cafe Code Brew gif",
       technology:[ "React.js", "Node.Js", "HTML", "CSS", "YELP and Twilio Api"," Semantic UI"]
     },
     {
     title:"3. Star Hopper",
     description:"A mock e-commerce website that sells all inclusive starship tickets to intergalatic stars. Take a trip to Orion and  explore meteor debris, star hiking and space games.",
-    url:"https://deeps-personal-website.s3.us-east-2.amazonaws.com/StarHopper.gif",
+    url:`${process.env.PUBLIC_URL}/assets/StarHopper.gif`,
     alt:"Star Hopper gif",
     technology:["PostgreSQL","Express","React.js","Redux", "Node.js", "Socket.io", "Oauth"]
     },
     {
     title:"4. Scrapplr",
     description:" A progressive web application to document your trip on the go. Include photos, text, and Google panoramic views. Track your trip with Google Map and share your photobook with your friends and family.",
-    url:"https://deeps-personal-website.s3.us-east-2.amazonaws.com/Scrapplr.gif",
+    url:`${process.env.PUBLIC_URL}/assets/Scrapplr.gif`,
     alt:"Scrapplr gif",
     technology:["Firebase", "React.js", "Node.js", "Html","CSS", "Google Maps API", "Grommet UI"],
     }
